65076768d269c0b0a5f0a4ae834414b31fedbaa6
Félix SCHERER <felix.scherer@laposte.net>
2025-10-13 10:42:11 +0100

    fix: build export

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  output: "export",
   images: {
     loader: "custom",
     formats: ["image/avif", "image/webp"],
+  },
+  eslint: {
+    dirs: ["app", "lib"],
   },
 };
